5015555d1a390b34674e6573aae5387680929984
wtimme <wtimme@users.noreply.github.com>
2020-08-12 17:50:16 +0200

    Add script for converting and combining all PDF files into one CSV

---
+++ convert-and-combine-all-pdf-files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# A bash script that converts all PDF documents into one single large CSV.
+
+# CONFIGURATION
+PDF_DIRECTORY="./pdf"
+CSV_DIRECTORY="./csv"
+OUTPUT_FILE="all.csv"
+
+# Remove the old output files.
+rm -f $OUTPUT_FILE
+rm -rf "$CSV_DIRECTORY"
+
+# Process all files
+for filename in $PDF_DIRECTORY/*
+do
+  bash pdf-to-csv.sh "$filename"
+done
+
+echo "Combining all CSV files into one..."
+cat $CSV_DIRECTORY/* >> $OUTPUT_FILE
+
+echo "Adding column title..."
+echo "Code,Result,Date"|cat - $OUTPUT_FILE > /tmp/out && mv /tmp/out $OUTPUT_FILE
+
+echo "Done."
