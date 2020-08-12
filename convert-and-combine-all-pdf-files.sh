eb7f1b5097e72069b2b878d9f7e34e6e6a50d794
wtimme <wtimme@users.noreply.github.com>
2020-08-12 20:42:02 +0200

    Ask Node web app to reload the CSV data once the PDFs were converted

--- convert-and-combine-all-pdf-files.sh
+++ convert-and-combine-all-pdf-files.sh
@@ -23,4 +23,7 @@ cat $CSV_DIRECTORY/* >> $OUTPUT_FILE
 echo "Adding column title..."
 echo "Code,Result,Date"|cat - $OUTPUT_FILE > /tmp/out && mv /tmp/out $OUTPUT_FILE
 
+echo "Asking the web server to reload the CSV data..."
+curl -s -o /dev/null -v http://localhost:1025/reload
+
 echo "Done."
