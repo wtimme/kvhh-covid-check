583ea2936a1e5ffe355811d2f9895ead78230496
wtimme <wtimme@users.noreply.github.com>
2020-08-13 08:12:04 +0200

    Store the date where the data was last updated to a text file

--- download-pdf-files.js
+++ download-pdf-files.js
@@ -7,6 +7,7 @@ const path = require("path");
 const base_url = 'https://www.kvhh.net'
 const url = base_url + '/kvhh/pages/index/p/1424'
 const pdf_directory = path.resolve(__dirname, 'pdf')
+const last_updated_file = path.resolve(__dirname, 'last-updated.txt')
 
 // Make sure the target directory exists.
 fs.mkdirSync(pdf_directory, { recursive: true });
@@ -14,6 +15,14 @@ fs.mkdirSync(pdf_directory, { recursive: true });
 rp(url)
 .then(function(html){
   const pdf_urls = new Set()
+
+  // Check when the data was last updated (according to their website)
+  last_updated_date = $('.table-doc-list h6 strong', html).text()
+  if (last_updated_date.length > 0) {
+    console.log('Last updated (according to them):', last_updated_date)
+
+    fs.writeFileSync(last_updated_file, last_updated_date, "UTF-8", {'flags': 'w'})
+  }
   
   const link_tags = $('.table-doc-list a', html)
   
