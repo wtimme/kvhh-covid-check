c4d5185095694bc270e6f26bc1c77085359e6f28
wtimme <wtimme@users.noreply.github.com>
2020-08-13 06:48:01 +0200

    Move convert-and-combine code into a function
    This prepares the merge of the two bash scripts.

--- convert-and-combine-all-pdf-files.sh
+++ convert-and-combine-all-pdf-files.sh
@@ -7,23 +7,27 @@ PDF_DIRECTORY="./pdf"
 CSV_DIRECTORY="./csv"
 OUTPUT_FILE="all.csv"
 
-# Remove the old output files.
-rm -f $OUTPUT_FILE
-rm -rf "$CSV_DIRECTORY"
+function convert_and_combine_all_files() {
+  # Remove the old output files.
+  rm -f $OUTPUT_FILE
+  rm -rf "$CSV_DIRECTORY"
 
-# Process all files
-for filename in $PDF_DIRECTORY/*
-do
-  bash pdf-to-csv.sh "$filename"
-done
+  # Process all files
+  for filename in $PDF_DIRECTORY/*
+  do
+    bash pdf-to-csv.sh "$filename"
+  done
 
-echo "Combining all CSV files into one..."
-cat $CSV_DIRECTORY/* >> $OUTPUT_FILE
+  echo "Combining all CSV files into one..."
+  cat $CSV_DIRECTORY/* >> $OUTPUT_FILE
 
-echo "Adding column title..."
-echo "Code,Result,Date"|cat - $OUTPUT_FILE > /tmp/out && mv /tmp/out $OUTPUT_FILE
+  echo "Adding column title..."
+  echo "Code,Result,Date"|cat - $OUTPUT_FILE > /tmp/out && mv /tmp/out $OUTPUT_FILE
 
-echo "Asking the web server to reload the CSV data..."
-curl -s -o /dev/null -v http://localhost:1025/reload
+  echo "Asking the web server to reload the CSV data..."
+  curl -s -o /dev/null -v http://localhost:1025/reload
 
-echo "Done."
+  echo "Done."
+}
+
+convert_and_combine_all_files
