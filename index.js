e78c24d3c85a70c9f5aef1385ae167bf04a9c549
wtimme <wtimme@users.noreply.github.com>
2020-08-13 08:22:07 +0200

    Collect variables passed to the template in one place

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use('/js', express.static('node_modules/@popperjs/core/dist/umd'))
 app.use('/js', express.static('node_modules/jquery/dist'))
 
 app.get('/', function (req, res) {
+  var templateVariables = {
+  }
+
   var rowWithCode;
   if (req.query.code && req.query.code.length > 0) {
     const code = req.query.code.trim()
@@ -26,17 +29,20 @@ app.get('/', function (req, res) {
       return row['Code'] === code
     })
 
+    // Allow the template to access the row's data.
+    templateVariables.row = rowWithCode
+
     if (!rowWithCode) {
-      res.render('no-result')
+      res.render('no-result', templateVariables)
     } else if (rowWithCode['Result'] == 'negativ') {
-      res.render('result-negative', { row: rowWithCode })
+      res.render('result-negative', templateVariables)
     } else if (rowWithCode['Result'] == 'positiv') {
-      res.render('result-positive', { row: rowWithCode })
+      res.render('result-positive', templateVariables)
     } else {
-      res.render('result-unknown', { row: rowWithCode })
+      res.render('result-unknown', templateVariables)
     }
   } else {
-    res.render('index')
+    res.render('index', templateVariables)
   }
 })
 
