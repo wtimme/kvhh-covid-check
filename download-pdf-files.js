858bea69f163eb3fda4eea1cb220cca3d0a4c186
wtimme <wtimme@users.noreply.github.com>
2020-08-23 02:17:12 +0200

    Fix download of all PDFs
    This makes sure the structural changes to the page of the KVHH
    are taken into account.

--- download-pdf-files.js
+++ download-pdf-files.js
@@ -130,47 +130,22 @@ downloadPDF = function(url) {
   })
 }
 
-rp(url)
-.then(function(html){
-  const pdf_urls = new Set()
-
-  // Check when the data was last updated (according to their website)
-  last_updated_date = $('.table-doc-list h6 strong', html).text()
-  if (last_updated_date.length > 0) {
-    console.log('Last updated (according to them):', last_updated_date)
-
-    fs.writeFileSync(last_updated_file, last_updated_date, "UTF-8", {'flags': 'w'})
-  }
-  
-  const link_tags = $('.table-doc-list a', html)
-  
-  for (let i = 0; i < link_tags.length; i++) {
-    const url = link_tags[i].attribs.href
-    const absolute_url = base_url + url
+getAllPDFURLs()
+  .then((urls) => {
+    console.log(`Starting to download ${urls.length} PDFs...`)
     
-    pdf_urls.add(absolute_url)
-    downloadFile(absolute_url)
-  }
-  
-  console.log('Downloaded', link_tags.length, 'files:');
-  console.log(pdf_urls)
-})
-.catch(function(err){
-  console.error('Failed to get HTML:', err)
-});
-
-var downloadFile = function(url, cb) {
-  var filename = path.basename(url)
-  var destination_path = pdf_directory + '/' + filename
-  
-  var file = fs.createWriteStream(destination_path);
-  var request = https.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });
-}
+    const promises = urls.map((url) => {
+      return downloadPDF(url)
+    })
+    
+    Promise.all(promises)
+      .then((results) => {
+        console.log('Downloads completed.')
+      })
+      .catch((err) => {
+        console.error('An error occurred.', err)
+      })
+  })
+  .catch((err) => {
+    console.error("Failed to download all PDFs.")
+  })
